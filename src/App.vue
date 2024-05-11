a227bd844c30dd092179a508cc0f9c4b89c87997
ShravaniMitkar <shravanimitkar443@gmail.com>
2024-05-11 16:58:44 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FormValidation from './components/FormValidation.vue'
+</script>
+<template>
+  <FormValidation />
+</template>
+
+<style>
+:root {
+  --mobile-label-font-size: 17px;
+  --mobile-label-placeholder-size: 15px;
+  --dark-blue: #0057ff;
+}
+</style>
